e9b15e18a2461666016975dcb5acf963d6f76b76
ANMOL RAJ <165140628+imanmolraj@users.noreply.github.com>
2025-08-01 01:15:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (72.47%) - LeetSync

---
+++ 807-custom-sort-string/custom-sort-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        vector<int> count(26,0);
+        for(int i =0;i<s.size();i++)
+        {
+            count[s[i]-'a']++;
+        }
+
+        string ans;
+        for(char i : order)
+        {
+            int char_ind= i-'a';
+            while(count[char_ind])
+            {
+                ans+=i;
+                count[char_ind]--;
+            }
+        }
+        for(int i=0;i<26;i++)
+        {
+            while(count[i])
+            {
+                char c = 'a'+i;
+                ans+=c;
+                count[i]--;
+            }
+        }
+        return ans;
+    }
+};
